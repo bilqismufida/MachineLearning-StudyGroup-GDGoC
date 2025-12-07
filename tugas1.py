e209b15e70f7cc416a2764464da7c4db025f1ae6
bilqismufida <bilqismufida207@gmail.com>
2025-12-07 21:19:08 +0700

    Remove files from root, keep only tugas1

--- tugas1.py
+++
@@ -1,8 +0,0 @@
-with open("data.txt") as file:
-    for n in file:
-        n = int(n)
-        
-        if n % 2 == 0:
-            print(f"{n} adalah genap")
-        else:
-            print(f"{n} adalah ganjil")
